2023a8c615132f0e369f4d6d5c0948615d791aa2
Aleksandr Butakov <anbngm@gmail.com>
2026-01-24 18:55:48 +0500

    deleted unnecessary files

--- hw02-collections-and-testing/src/main/java/ru/anbn/hw02/demo/generics/bounds/entries/Animal.java
+++
@@ -1,8 +0,0 @@
-package ru.anbn.hw02.demo.generics.bounds.entries;
-
-public class Animal {
-    @Override
-    public String toString() {
-        return "Animal";
-    }
-}
--- hw02-collections-and-testing/src/main/java/ru/anbn/hw02/demo/generics/bounds/entries/SiberianCat.java
+++
@@ -1,3 +0,0 @@
-package ru.anbn.hw02.demo.generics.bounds.entries;
-
-public class SiberianCat extends Cat {}
